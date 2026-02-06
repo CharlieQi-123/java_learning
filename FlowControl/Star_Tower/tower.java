e7976496a66969a2215bea5095a995deaf37a015
CharlieQi-123 <charlie.qiding@gmail.com>
2026-02-06 12:19:56 -0500

    Add Star Tower pattern implementation in tower.java

---
+++ FlowControl/Star_Tower/tower.java
@@ -0,0 +1,14 @@
+public class FlowControl_practice {
+    public static void main(String[] args) {
+        int level = 9;
+        for (int i = 0;i < level;i++){
+            for (int j = 0;j < (level - 1) - i; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0;k < i;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
